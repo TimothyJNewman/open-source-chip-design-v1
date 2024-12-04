c2f9fff35aed958492642904228c25a8a49c2786
TimothyJNewman <timothyjabeznewman@gmail.com>
2024-12-04 00:32:58 +0000

    Added automated image export and modified libs tech folder

---
+++ Automated_Image_Export/xschem_commands.tcl
@@ -0,0 +1,42 @@
+xschem hilight_instname M10
+xschem print png m10_open_drain_none_0.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M7
+xschem print png m7_short_drain_source_5.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M9
+xschem print png m9_short_gate_source_9.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M8
+xschem print png m8_open_drain_none_11.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M2
+xschem print png m2_short_gate_drain_17.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M10
+xschem print png m10_open_source_none_21.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M8
+xschem print png m8_open_gate_none_23.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M7
+xschem print png m7_open_source_none_24.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M9
+xschem print png m9_open_source_none_27.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M9
+xschem print png m9_open_drain_none_31.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M8
+xschem print png m8_short_gate_source_34.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M10
+xschem print png m10_short_drain_source_49.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M8
+xschem print png m8_open_source_none_56.png 0 0 -800 -650 650 200
+xschem unhilight_all
+xschem hilight_instname M6
+xschem print png m6_short_gate_drain_58.png 0 0 -800 -650 650 200
+xschem unhilight_all
